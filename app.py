c5de05177cba1821bd003892b694477058a08205
Jungu Lee <heback@gmail.com>
2022-08-08 08:55:43 +0900

    initial committment

--- app.py
+++ app.py
@@ -278,3 +278,25 @@ with tab3:
         if logout_btn:
             del st.session_state['user_id']
             st.experimental_rerun()
+
+
+hide_streamlit_style = """
+	<style>
+	/* This is to hide hamburger menu completely */
+	#MainMenu {visibility: hidden;}
+	/* This is to hide Streamlit footer */
+	footer {visibility: hidden;}
+	/*
+	If you did not hide the hamburger menu completely,
+	you can use the following styles to control which items on the menu to hide.
+	*/
+	ul[data-testid=main-menu-list] > li:nth-of-type(4), /* Documentation */
+	ul[data-testid=main-menu-list] > li:nth-of-type(5), /* Ask a question */
+	ul[data-testid=main-menu-list] > li:nth-of-type(6), /* Report a bug */
+	ul[data-testid=main-menu-list] > li:nth-of-type(7), /* Streamlit for Teams */
+	ul[data-testid=main-menu-list] > div:nth-of-type(2) /* 2nd divider */
+		{display: none;}
+	</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
